7da0d1c4e69bae146f82fb86bb934b5dde8a29d8
Snehal Reddy <syelmat@gmail.com>
2020-03-24 00:15:12 +0530

    routes and navigation drawer updated

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,19 @@
 <template>
   <v-app>
-
-    <v-content>
+    <Navbar />
+    <v-content class="mx-4">
       <router-view></router-view>
     </v-content>
-  
   </v-app>
 </template>
 
 <script>
+import Navbar from "@/components/Navbar";
+
 export default {
-  name: "App"
+  name: "App",
+  components: {
+    Navbar
+  }
 };
 </script>
---
+++ src/components/Navbar.vue
@@ -0,0 +1,53 @@
+<template>
+  <nav>
+    <v-toolbar flat app class="grey lighten-3">
+      <v-app-bar-nav-icon
+        class="grey--text"
+        @click="drawer = !drawer"
+      ></v-app-bar-nav-icon>
+
+      <v-toolbar-title class="text-uppercase grey--text">
+        <span class="font-weight-light">Vue</span>
+        <span>Do</span>
+      </v-toolbar-title>
+
+      <v-spacer></v-spacer>
+
+      <v-btn flat color="grey lighten-3">
+        <span>Sign Out</span>
+        <v-icon right>mdi-exit-to-app</v-icon>
+      </v-btn>
+    </v-toolbar>
+
+    <v-navigation-drawer v-model="drawer" app class="blue lighten-1">
+      <v-list>
+        <v-list-item v-for="link in links" :key="link.text" router :to="link.route">
+          <v-list-item-icon
+            ><v-icon class="white--text">{{ link.icon }}</v-icon>
+          </v-list-item-icon>
+          <v-list-item-content class="white--text">
+            <v-list-item-title>{{ link.text }}</v-list-item-title>
+          </v-list-item-content>
+        </v-list-item>
+      </v-list>
+    </v-navigation-drawer>
+  </nav>
+</template>
+
+<script>
+export default {
+  name: "navbar",
+  data() {
+    return {
+      drawer: false,
+      links: [
+        {icon: 'mdi-view-dashboard', text: 'Dashboard', route: '/'},
+        {icon: 'mdi-folder-open', text: 'My Projects', route: '/projects'},
+        {icon: 'mdi-account-multiple', text: 'Team', route: '/team'}
+      ]
+    };
+  }
+};
+</script>
+
+<style scoped></style>
